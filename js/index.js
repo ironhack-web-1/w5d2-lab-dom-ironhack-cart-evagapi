dc1b01f93c575a1dc19db54a50de8763ff2588e0
Eva Garcia Pinos <eva.gapi@gmail.com>
2022-08-07 21:03:22 +0200

    feat: allow users to add custom products

--- js/index.js
+++ js/index.js
@@ -2,8 +2,6 @@
 
 //Calculates subtotal price of every product
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
   //... your code goes here
   const price = product.querySelector('.price span');
   const quantity = product.querySelector('.quantity input');
@@ -45,8 +43,51 @@ function removeProduct(event) {
 
 // ITERATION 5
 
+function getProductName() {
+  const nameInput = document.getElementById('create-product-name');
+  return nameInput.value;
+}
+
+function getProductPrice() {
+  const priceInput = document.getElementById('create-product-price');
+  return parseFloat(priceInput.value);
+}
+
+function addNewProductToCart(name, price) {
+  const cartBody = document.querySelector('#cart tbody');
+  const productRow = document.createElement('tr');
+  productRow.classList.add('product');
+  productRow.innerHTML += `
+<td class="name">
+  <span>${name}</span>
+</td>
+<td class="price">$<span>${price}</span></td>
+<td class="quantity">
+  <input type="number" value="0" min="0" placeholder="Quantity" />
+</td>
+<td class="subtotal">$<span>0</span></td>
+<td class="action">
+  <button class="btn btn-remove">Remove</button>
+</td>
+`;
+  const removeButton = productRow.querySelector('.btn-remove');
+  removeButton.addEventListener('click', removeProduct);
+
+  cartBody.appendChild(productRow);
+}
+
 function createProduct() {
   //... your code goes here
+  const nameInput = document.getElementById('create-product-name');
+  const productName = nameInput.value;
+
+  const priceInput = document.getElementById('create-product-price');
+  const productPrice = parseFloat(priceInput.value);
+
+  addNewProductToCart(productName, productPrice);
+
+  nameInput.value = '';
+  priceInput.value = 0;
 }
 
 window.addEventListener('load', () => {
@@ -60,4 +101,7 @@ window.addEventListener('load', () => {
     const removeButton = removeButtons[index];
     removeButton.addEventListener('click', removeProduct);
   }
+
+  const createProductButton = document.getElementById('create');
+  createProductButton.addEventListener('click', createProduct);
 });
